dc371b466bff4fe68a34549a7439cc45b1e64307
João Bruno <jpedrobruno@hotmail.com>
2020-12-15 22:22:04 -0300

    test: validate null and empty emails

---
+++ src/entities/email-validation.spec.ts
@@ -0,0 +1,18 @@
+import { Email } from '@entities/email'
+
+describe('Email validation', () => {
+  test('should not expect null strings', () => {
+    const email = null
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should not accept empty strings', () => {
+    const email = ''
+    expect(Email.validate(email)).toBeFalsy()
+  })
+
+  test('should accept valid email', () => {
+    const email = 'any@mail.com'
+    expect(Email.validate(email)).toBeTruthy()
+  })
+})
