b7a8da209fb4f126d914e0f2c95d03687e42af34
João Bruno <jpedrobruno@hotmail.com>
2020-12-23 20:22:44 -0300

    feat: verify name when creating user

--- src/entities/user.ts
+++ src/entities/user.ts
@@ -1,10 +1,18 @@
 import { UserData } from '@entities/user-data'
 import { Either, left } from '@shared/either'
-import { InvalidEmailError } from '@entities/errors/invalid-email-error'
 import { Email } from '@entities/email'
+import { Name } from '@entities/name'
+
+import { InvalidNameError } from '@entities/errors/invalid-name-error'
+import { InvalidEmailError } from '@entities/errors/invalid-email-error'
 
 export class User {
-  static create (userData: UserData): Either<InvalidEmailError, User> {
+  static create (userData: UserData): Either<InvalidNameError | InvalidEmailError, User> {
+    const nameOrError = Name.create(userData.name)
+    if (nameOrError.isLeft()) {
+      return left(new InvalidNameError())
+    }
+
     const emailOrError = Email.create(userData.email)
     if (emailOrError.isLeft()) {
       return left(new InvalidEmailError())
