6861e0986f3c0f5a9260ab8fa2e8513fb91a1bf9
João Bruno <jpedrobruno@hotmail.com>
2020-12-23 20:03:29 -0300

    feat: either type, right and left classes

--- jest.config.js
+++ jest.config.js
@@ -50,6 +50,7 @@ module.exports = {
   // A map from regular expressions to module names or to arrays of module names that allow to stub out resources with a single module
   moduleNameMapper: {
     '^@ports/(.*)$': '<rootDir>/src/ports/$1',
+    '^@shared/(.*)$': '<rootDir>/src/shared/$1',
     '^@entities/(.*)$': '<rootDir>/src/entities/$1',
     '^@repositories/(.*)$': '<rootDir>/src/repositories/$1'
   }
