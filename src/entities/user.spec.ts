fe60e712ffd0fe5e371373d0c6844210aff93a65
João Bruno <jpedrobruno@hotmail.com>
2020-12-23 20:04:56 -0300

    test: verify e-mail address when creating user

---
+++ src/entities/user.spec.ts
@@ -0,0 +1,12 @@
+import { User } from '@entities/user'
+import { left } from '@shared/either'
+import { InvalidEmailError } from '@entities/errors/invalid-email-error'
+
+describe('User domain entity', () => {
+  test('should not create user with invalid e-mail address', () => {
+    const invalidEmail = 'invalid_email'
+    const error = User.create({ name: 'any_name', email: invalidEmail })
+
+    expect(error).toEqual(left(new InvalidEmailError()))
+  })
+})
