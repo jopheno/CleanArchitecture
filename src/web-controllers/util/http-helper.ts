ab53838a6a18cc78b0b59658487ea668d25d5a48
João Bruno <jpedrobruno@hotmail.com>
2021-02-06 20:07:42 -0300

    feat: web controller should return 400 when user has invalid name or email

--- src/web-controllers/util/http-helper.ts
+++ src/web-controllers/util/http-helper.ts
@@ -1,18 +1,11 @@
-/*
-const httpReponse: HttpResponse = {
-  statusCode: 201,
-  body: {
-    name: response.value.name,
-    email: response.value.email
-  }
-}
-
-return httpReponse
-*/
-
 import { HttpResponse } from '@/web-controllers/ports'
 
 export const created = (data: any): HttpResponse => ({
   statusCode: 201,
+  body: data
+})
+
+export const badRequest = (data: any): HttpResponse => ({
+  statusCode: 400,
   body: data
 })
